d556228d962c5bf31b495cb1a06b1e7522fd3364
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2024-04-24 01:40:36 +0530

    done request-header-parser

--- request-header-parser-microservice/src/index.js
+++ request-header-parser-microservice/src/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
-const { router } = require("./src/routes");
+const { router } = require("./routes");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1", router);
+app.use("/api/", router);
 
 app.listen(PORT, () => {
-  console.log("Timestamp service listening on port " + PORT);
+  console.log("Request header parser service listening on port ", PORT);
 });
---
+++ timestamp-microservice/src/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const { router } = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/", router);
+
+app.listen(PORT, () => {
+  console.log("Timestamp service listening on port " + PORT);
+});
